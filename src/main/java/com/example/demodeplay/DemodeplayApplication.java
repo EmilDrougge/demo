3b97251774d453b22d391a495065a16cdb8d4c60
EmilDrougge <svenemildrougge@gmail.com>
2026-09-03 10:09:22 +0200

    Commit

---
+++ src/main/java/com/example/demodeplay/DemodeplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodeplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodeplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemodeplayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demodeplay/DemodeplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodeplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodeplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
